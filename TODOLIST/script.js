2ce3e42f4e9b6956a0de733c8af0310d4c9d524e
Sahil-Iqubal <sahiliqubal06@gmail.com>
2025-04-11 16:28:06 +0530

    Updated js

--- TODOLIST/script.js
+++ TODOLIST/script.js
@@ -18,12 +18,33 @@ document.addEventListener("DOMContentLoaded", () => {
     };
     tasks.push(newTask);
     saveTasks();
+    renderTask(newTask);
     todoInput.value = "";
     console.log(tasks);
   });
 
   function renderTask(task) {
-    console.log(task);
+    const li = document.createElement("li");
+    li.setAttribute("data-d", task.id);
+    if (task.completed) li.classList.add("completed");
+    li.innerHTML = `<span>${task.text}</span>
+    <button>Delete</button>
+    `;
+    li.addEventListener("click", (e) => {
+      if (e.target.tagName === "BUTTON") return;
+      task.completed = !task.completed;
+      li.classList.toggle("completed");
+      saveTasks();
+    });
+
+    li.querySelector("button").addEventListener("click", (e) => {
+      e.stopPropagation();
+      tasks = tasks.filter((t) => t.id !== task.id);
+      li.remove();
+      saveTasks();
+    });
+
+    todoList.appendChild(li);
   }
 
   function saveTasks() {
