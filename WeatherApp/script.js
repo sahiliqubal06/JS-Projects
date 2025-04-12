a10a1d773948a500bf655efe2ece4af22251b2e0
Sahil-Iqubal <sahiliqubal06@gmail.com>
2025-04-12 15:11:19 +0530

    Added script.js

---
+++ WeatherApp/script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cityInput = document.getElementById("city-input");
+  const getWeatherBtn = document.getElementById("get-weather-btn");
+  const weatherInfo = document.getElementById("weather-info");
+  const cityName = document.getElementById("city-name");
+  const temperatureDisplay = document.getElementById("temperature");
+  const descriptionDisplay = document.getElementById("description");
+  const errorMessage = document.getElementById("error-message");
+
+  const API_KEY = "e99adab4c9a2d8ff477639a7f5d7a382";
+
+  getWeatherBtn.addEventListener("click", async () => {
+    const city = cityInput.value.trim();
+    if (!city) return;
+
+    try {
+      const weatherData = await fetchWeatherData(city);
+      displayWeatherData(weatherData);
+    } catch (error) {
+      showError();
+    }
+  });
+
+  async function fetchWeatherData(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+
+    const response = await fetch(url);
+    console.log(typeof response);
+    console.log("RESPONSE", response);
+  }
+
+  function displayWeatherData(data) {}
+
+  function showError() {
+    weatherInfo.classList.add("hidden");
+    errorMessage.classList.remove("hidden");
+  }
+});
