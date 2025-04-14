a7bde670900cbc26028ebe269f90ab7dbb7bf912
Sahil-Iqubal <sahiliqubal06@gmail.com>
2025-04-14 21:07:33 +0530

    Added Expense Tracker functionality with form submission, expense list rendering, and total amount calculation and display

---
+++ ExpenseTracker/script.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const expenseForm = document.getElementById("expense-form");
+  const expenseName = document.getElementById("expense-name");
+  const expenseAmount = document.getElementById("expense-amount");
+  const expenseList = document.getElementById("expense-list");
+  const totalexpense = document.getElementById("total");
+  const totalAmountDisplay = document.getElementById("total-amount");
+
+  let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+  let totalAmount = calculateTotal();
+
+  renderExpenses();
+
+  expenseForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = expenseName.value.trim();
+    const amount = parseFloat(expenseAmount.value.trim());
+
+    if (name !== "" && !isNaN(amount) && amount > 0) {
+      const newExpense = {
+        id: Date.now(),
+        name,
+        amount,
+      };
+      expenses.push(newExpense);
+      saveExpensesToLocal();
+      renderExpenses();
+      updateTotal();
+
+      //   CLear Input
+      expenseName.value = "";
+      expenseAmount.value = "";
+    }
+  });
+
+  function renderExpenses() {
+    expenseList.innerHTML = "";
+    expenses.forEach((expense) => {
+      const li = document.createElement("li");
+      li.innerHTML = `${expense.name} - $${expense.amount}
+        <button data-id="${expense.id}">Delete</button>`;
+      expenseList.appendChild(li);
+    });
+  }
+
+  function calculateTotal() {
+    return expenses.reduce((sum, expense) => sum + expense.amount, 0);
+  }
+
+  function updateTotal() {
+    totalAmount = calculateTotal();
+    totalAmountDisplay.textContent = totalAmount.toFixed(2);
+  }
+
+  function saveExpensesToLocal() {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }
+
+  expenseList.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      const expenseId = parseInt(e.target.getAttribute("data-id"));
+      expenses = expenses.filter((expense) => expense.id !== expenseId);
+      saveExpensesToLocal();
+      renderExpenses();
+      updateTotal();
+    }
+  });
+});
