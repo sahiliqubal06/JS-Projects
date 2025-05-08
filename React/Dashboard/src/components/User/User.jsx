e067a4761efef616d8e1052b85b69c7249515221
Sahil-Iqubal <sahiliqubal06@gmail.com>
2025-05-08 23:23:15 +0530

    Added new react project

---
+++ React/Dashboard/src/components/User/User.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function User() {
+  return (
+    <div className="bg-orange-500 text-black text-3xl text-center py-5">
+      User
+      <p></p>
+    </div>
+  );
+}
+
+export default User;
